26176cd4dde7ec799de32bb59b716e81328dc613
ValeevDA <valeevdmitriy@gmail.com>
2020-07-20 02:25:31 +1000

    added changing game speed

---
+++ Assets/Scripts/GameSpeedChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class GameSpeedChanger : MonoBehaviour
+{    
+    public Slider slider;
+
+    private void OnEnable()
+    {
+        slider.onValueChanged.AddListener(ChangeGameSpeed);    
+    }
+
+    private void OnDisable()
+    {
+        slider.onValueChanged.RemoveListener(ChangeGameSpeed);    
+    }
+
+    public void ChangeGameSpeed(float value)
+    {
+        Time.timeScale = value;
+    }
+}
